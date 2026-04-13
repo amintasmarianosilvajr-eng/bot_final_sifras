62a6a121dfb5cc1596d5a19b60ffd4897d6f7409
Sifras Deploy <deploy@sifras.bot>
2026-04-13 10:05:20 -0300

    fix: total virgin isolation v8.6.5 - no local loading for clients

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
-const VERSION = "v8.6.4 (VIRGIN_SAFE)";
+const VERSION = "v8.6.5 (TOTAL_VIRGIN)";
 
 // --- CONFIGURAÇÃO DE PERSISTÊNCIA ---
 const VOLUME_PATH = '/app/data';
